dc7e67d1c82c8a98d642bf0d7e2365a79b57a6e4
DAMIAAN\Siebe <siebe.clerinx@outlook.com>
2019-03-02 00:19:01 +0100

    First profile commit

--- app/src/main/java/com/example/siebster/eventfriend/ProfileActivity.java
+++ app/src/main/java/com/example/siebster/eventfriend/ProfileActivity.java
@@ -1,8 +1,16 @@
 package com.example.siebster.eventfriend;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Siebe on 1/03/2019.
  */
 
-public class ProfileActivity {
-}
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
